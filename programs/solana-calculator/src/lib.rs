9118c461b918fa7dab743879f449472f77e0968c
robertohuertasm <roberto.huertas@outlook.com>
2021-12-11 00:02:28 +0100

    simplify ops

--- programs/solana-calculator/src/lib.rs
+++ programs/solana-calculator/src/lib.rs
@@ -12,25 +12,25 @@ pub mod solana_calculator {
         Ok(())
     }
 
-    pub fn add(ctx: Context<Addition>, num1: i64, num2: i64) -> ProgramResult {
+    pub fn add(ctx: Context<Operation>, num1: i64, num2: i64) -> ProgramResult {
         let calculator = &mut ctx.accounts.calculator;
         calculator.result = i64::saturating_add(num1, num2);
         Ok(())
     }
 
-    pub fn multiply(ctx: Context<Multiplication>, num1: i64, num2: i64) -> ProgramResult {
+    pub fn multiply(ctx: Context<Operation>, num1: i64, num2: i64) -> ProgramResult {
         let calculator = &mut ctx.accounts.calculator;
         calculator.result = i64::saturating_mul(num1, num2);
         Ok(())
     }
 
-    pub fn subtract(ctx: Context<Subtraction>, num1: i64, num2: i64) -> ProgramResult {
+    pub fn subtract(ctx: Context<Operation>, num1: i64, num2: i64) -> ProgramResult {
         let calculator = &mut ctx.accounts.calculator;
         calculator.result = i64::saturating_sub(num1, num2);
         Ok(())
     }
 
-    pub fn divide(ctx: Context<Division>, num1: i64, num2: i64) -> ProgramResult {
+    pub fn divide(ctx: Context<Operation>, num1: i64, num2: i64) -> ProgramResult {
         let calculator = &mut ctx.accounts.calculator;
         calculator.result = num1 / num2;
         calculator.remainder = num1 % num2;
@@ -48,25 +48,7 @@ pub struct Create<'info> {
 }
 
 #[derive(Accounts)]
-pub struct Addition<'info> {
-    #[account(mut)]
-    pub calculator: Account<'info, Calculator>,
-}
-
-#[derive(Accounts)]
-pub struct Multiplication<'info> {
-    #[account(mut)]
-    pub calculator: Account<'info, Calculator>,
-}
-
-#[derive(Accounts)]
-pub struct Subtraction<'info> {
-    #[account(mut)]
-    pub calculator: Account<'info, Calculator>,
-}
-
-#[derive(Accounts)]
-pub struct Division<'info> {
+pub struct Operation<'info> {
     #[account(mut)]
     pub calculator: Account<'info, Calculator>,
 }
